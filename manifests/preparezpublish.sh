35d7a5bbf8b4375f8d70a19c716a6e69f54cef70
Pedro Resende <pedro.resende@cleverti.com>
2013-07-12 08:29:14 +0100

    Added a missing verification to assure that if ezpublish is already present it won't be override when executing for the second time a vagrant up

--- manifests/preparezpublish.sh
+++ manifests/preparezpublish.sh
@@ -2,6 +2,7 @@
 
 WWW=/var/www/html
 EZPUBLISH=http://share.ez.no/content/download/151343/893911/version/1/file/ezpublish5_community_project-2013.06-gpl-full.tar.gz
+EZPUBLISH_FOLDER=ezpublish5
 
 function geteZPublish {
     cd $WWW
@@ -15,24 +16,26 @@ function extracteZPublish {
 
 function rename {
     cd $WWW
-    /bin/mv /var/www/html/ezpublish5_community_project-2013.6-gpl-full/ /var/www/html/ezpublish5
+    /bin/mv /var/www/html/ezpublish5_community_project-2013.6-gpl-full/ $WWW/$EZPUBLISH_FOLDER
 }
 
 function preparePermissions {
-    cd $WWW/ezpublish5
+    cd $WWW/$EZPUBLISH_FOLDER
     /usr/bin/setfacl -R -m u:apache:rwx -m u:apache:rwx ezpublish/{cache,logs,config} ezpublish_legacy/{design,extension,settings,var} web
     /usr/bin/setfacl -dR -m u:apache:rwx -m u:vagrant:rwx ezpublish/{cache,logs,config} ezpublish_legacy/{design,extension,settings,var} web
 }
 
 function assets {
-    cd $WWW/ezpublish5
+    cd $WWW/$EZPUBLISH_FOLDER
     /usr/bin/php ezpublish/console assets:install --symlink web
     /usr/bin/php ezpublish/console ezpublish:legacy:assets_install --symlink web
     /usr/bin/php ezpublish/console assetic:dump --env=prod web
 }
 
-geteZPublish
-extracteZPublish
-rename
-preparePermissions
-assets
+if [ ! -d $WWW/$EZPUBLISH_FOLDER ]; then
+    geteZPublish
+    extracteZPublish
+    rename
+    preparePermissions
+    assets
+fi
