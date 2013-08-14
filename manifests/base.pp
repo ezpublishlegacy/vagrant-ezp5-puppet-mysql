51fb9a0a573af234b4d71bd0f16e3ef027a5560e
Pedro Resende <pedroresende@mail.resende.biz>
2013-08-14 16:18:02 +0100

    Moved the requirement of the virtualhosts, before starting apache

--- manifests/base.pp
+++ manifests/base.pp
@@ -206,12 +206,13 @@ class prepareezpublish {
     service { 'httpd':
       ensure => running,
       enable => true,
-      before => Exec["prepare eZ Publish"]
+      before => Exec["prepare eZ Publish"],
+      require => [File['/etc/httpd/conf.d/01.accept_pathinfo.conf'], File['/etc/httpd/conf.d/ezp5.conf']]
     } ~>
     exec    { "prepare eZ Publish":
       command => "/bin/bash /tmp/vagrant-puppet/manifests/preparezpublish.sh",
       path    => "/usr/local/bin/:/bin/",
-      require => [Package["httpd", "php-cli", "php-gd" ,"php-mysqlnd", "php-pear", "php-xml", "php-mbstring", "php"], File['/etc/httpd/conf.d/01.accept_pathinfo.conf'], File['/etc/httpd/conf.d/ezp5.conf']]
+      require => Package["httpd", "php-cli", "php-gd" ,"php-mysqlnd", "php-pear", "php-xml", "php-mbstring", "php"]
     }
 }
 
