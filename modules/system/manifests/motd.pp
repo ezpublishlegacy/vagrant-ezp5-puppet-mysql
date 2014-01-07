dada2dceef36c0e044d5a1740e7f860fd7e6cec2
Pedro Resende <pedroresende@mail.resende.biz>
2014-01-07 13:18:57 +0000

    Removed extra / in modules/system/manifests/motd.pp

--- modules/system/manifests/motd.pp
+++ modules/system/manifests/motd.pp
@@ -1,7 +1,7 @@
 class system::motd {
     file    {'/etc/motd':
       ensure  => file,
-      content => template('/tmp/vagrant-puppet/modules-0/system/manifests/files//motd.erb'),
+      content => template('/tmp/vagrant-puppet/modules-0/system/manifests/files/motd.erb'),
       owner   => 'root',
       group   => 'root',
       mode    => '644',
