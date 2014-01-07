74120747494bd40408a1f7e3fba1eca9d34a507b
Pedro Resende <pedroresende@mail.resende.biz>
2014-01-07 11:00:36 +0000

    Fix Git installation

--- modules/ezpublish/manifests/install.pp
+++ modules/ezpublish/manifests/install.pp
@@ -3,60 +3,60 @@ class ezpublish::install {
   $ezpublish_src = 'http://share.ez.no/content/download/154571/912584/version/1/file/ezpublish5_community_project-2013.11-gpl-full.tar.gz'
   $ezpublish_folder = 'ezpublish5'
   $ezpublish = 'ezpublish.tar.gz'
-  $type = 'tar' # This can be tar, zip or git if you're using base_xdedug
+  $type = 'git' # This can be tar, zip or git if you're using base_xdedug
   ezpublish::install::fetch{ 'fetch':
     www => $www, 
     ezpublish_src => $ezpublish_src, 
     ezpublish_folder => $ezpublish_folder, 
     ezpublish => $ezpublish, 
     type => $type
-  } ->
+  } ~>
   exec { "setfacl-R":
     command => "/usr/bin/setfacl -R -m u:apache:rwx -m u:apache:rwx $www/$ezpublish_folder/ezpublish/{cache,logs,config} $www/$ezpublish_folder/ezpublish_legacy/{design,extension,settings,var} web",
     onlyif  => '/usr/bin/test -d $www/$ezpublish_folder',
     returns => [ 1 ]
-  } ->
+  } ~>
   exec { "setfacl-dR":
     command => "/usr/bin/setfacl -dR -m u:apache:rwx -m u:vagrant:rwx $www/$ezpublish_folder/ezpublish/{cache,logs,config} $www/$ezpublish_folder/ezpublish_legacy/{design,extension,settings,var} web",
     onlyif  => '/usr/bin/test -d $www/$ezpublish_folder',
     returns => [ 0, 1, 2, '', ' ']
-  } ->
+  } ~>
   exec { "remove_cache":
     command => "/bin/rm -rf $www/$ezpublish_folder/ezpublish/cache/*",
     onlyif  => '/usr/bin/test -d $www/$ezpublish_folder'
-  } ->
+  } ~>
   file { "$www/$ezpublish_folder/ezpublish/config/ezpublish_prod.yml":
     ensure  => file,
     content => template('/tmp/vagrant-puppet/modules-0/ezpublish/manifests/setup/ezpublish_prod.yml.erb'),
     owner   => 'apache',
     group   => 'apache',
     mode    => '666',
-  } ->
+  } ~>
   file { "$www/$ezpublish_folder/ezpublish/config/ezpublish_dev.yml":
     ensure => file,
     content => template('/tmp/vagrant-puppet/modules-0/ezpublish/manifests/setup/ezpublish_prod.yml.erb'),
     owner   => 'apache',
     group   => 'apache',
     mode    => '666',
-  } ->
+  } ~>
   file { "$www/$ezpublish_folder/ezpublish/config/ezpublish.yml":
     ensure => file,
     content => template('/tmp/vagrant-puppet/modules-0/ezpublish/manifests/setup/ezpublish.yml.erb'),
     owner   => 'apache',
     group   => 'apache',
     mode    => '666',
-  } ->
+  } ~>
   exec { "assets_install":
     command => "/usr/bin/php $www/$ezpublish_folder/ezpublish/console assets:install --symlink $www/$ezpublish_folder/web",
-    onlyif  => '/usr/bin/test -d $www/$ezpublish_folder',
-  } ->
+    onlyif  => ['/usr/bin/test -d $www/$ezpublish_folder','/usr/bin/test -d $www/$ezpublish_folder/vendor'],
+  } ~>
   exec { "legacy_assets":
     command => "/usr/bin/php $www/$ezpublish_folder/ezpublish/console ezpublish:legacy:assets_install --symlink $www/$ezpublish_folder/web",
-    onlyif  => '/usr/bin/test -d $www/$ezpublish_folder',
+    onlyif  => ['/usr/bin/test -d $www/$ezpublish_folder','/usr/bin/test -d $www/$ezpublish_folder/vendor'],
   } ~>
   exec { "assetic_dump":
     command => "/usr/bin/php $www/$ezpublish_folder/ezpublish/console assetic:dump",
-    onlyif  => '/usr/bin/test -d $www/$ezpublish_folder',
+    onlyif  => ['/usr/bin/test -d $www/$ezpublish_folder','/usr/bin/test -d $www/$ezpublish_folder/vendor'],
   } ~>
   exec { "create_folders":
     command => "/bin/mkdir -p $www/$ezpublish_folder/ezpublish_legacy/settings/override && /bin/mkdir -p $www/$ezpublish_folder/ezpublish_legacy/settings/siteaccess/eng && /bin/mkdir -p $www/$ezpublish_folder/ezpublish_legacy/settings/siteaccess/ezdemo_site && /bin/mkdir -p $www/$ezpublish_folder/ezpublish_legacy/settings/siteaccess/ezdemo_site_admin",
@@ -102,5 +102,6 @@ class ezpublish::install {
     command => "/usr/bin/php bin/php/ezpgenerateautoloads.php --extension",
     cwd  => "$www/$ezpublish_folder/ezpublish_legacy",
     path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:$www/$ezpublish_folder",
+    onlyif  => ['/usr/bin/test -d $www/$ezpublish_folder','/usr/bin/test -d $www/$ezpublish_folder/vendor'],
   }
 }
--- modules/ezpublish/manifests/install/fetch/git.pp
+++ modules/ezpublish/manifests/install/fetch/git.pp
@@ -1,17 +1,20 @@
-define ezpublish::install::fetch::git ($www, $ezpublish_src, $ezpublish_folder) {
+define ezpublish::install::fetch::git ($www, $ezpublish_folder) {
   exec { "clone-ezpublish-community":
     command => "/usr/bin/git clone https://github.com/ezsystems/ezpublish-community.git $www/$ezpublish_folder",
-    refreshonly => true,
     returns => [ 0, 1, 2, '', ' ']
   } ~>
   exec { "clone-ezpublish-legacy":
     command => "/usr/bin/git clone https://github.com/ezsystems/ezpublish-legacy.git ezpublish_legacy",
     cwd  => "$www/$ezpublish_folder",
     path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:$www/$ezpublish_folder",
+    require => Exec["clone-ezpublish-community"],
+    returns => [ 0, 128 ]
   } ~>
   exec { "composer-install":
     command => "/usr/bin/php /usr/local/bin/composer.phar install --prefer-dist",
     cwd  => "$www/$ezpublish_folder",
     path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:$www/$ezpublish_folder",
+    require => [Exec["clone-ezpublish-community"], Exec["clone-ezpublish-legacy"]],
+    returns => [ 0, 1 ]
   }
 }
