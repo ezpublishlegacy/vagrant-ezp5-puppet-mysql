c080f230c3ba428997a5ae770a3e9ca1abab82cc
Pedro Resende <pedroresende@mail.resende.biz>
2014-01-08 10:41:11 +0000

    Added bash script for fetch git

--- modules/ezpublish/manifests/install/fetch/git.pp
+++ modules/ezpublish/manifests/install/fetch/git.pp
@@ -1,21 +1,6 @@
 define ezpublish::install::fetch::git ($www, $ezpublish_folder) {
-  exec { "clone-ezpublish-community":
-    command => "/usr/bin/git clone https://github.com/ezsystems/ezpublish-community.git $www/$ezpublish_folder",
-    returns => [ 0, 1, 2, '', ' ']
-  } ~>
-  exec { "clone-ezpublish-legacy":
-    command => "/usr/bin/git clone https://github.com/ezsystems/ezpublish-legacy.git ezpublish_legacy",
-    cwd     => "$www/$ezpublish_folder",
-    path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:$www/$ezpublish_folder",
-    require => Exec["clone-ezpublish-community"],
-    returns => [ 0, 128 ]
-  } ~>
-  exec { "composer-install":
-    command => "/usr/bin/php /usr/local/bin/composer.phar install --prefer-dist",
-    cwd     => "$www/$ezpublish_folder",
-    path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:$www/$ezpublish_folder",
-    require => Exec["clone-ezpublish-community"],
-    before  => [Exec["assets_install"], Exec["legacy_assets"], Exec["assetic_dump"], Exec["regenerateautoloads"]],
-    returns => [ 0, 1 ]
+  exec { "prepare eZ Publish":
+    command => "/bin/bash /tmp/vagrant-puppet/modules-0/ezpublish/manifests/setup/preparezpublish.sh $www $ezpublish_folder",
+    path    => "/usr/local/bin/:/bin/"
   }
 }
